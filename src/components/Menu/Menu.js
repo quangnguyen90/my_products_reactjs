091667bd73ec45042f0e2400fa678bd26f46db27
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-09-15 23:30:08 +0700

    3.3- React router for Custom Menu Link

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const menus = [
+    {
+        name: 'Home Page',
+        to: '/',
+        exact: true
+    },
+    {
+        name: 'Products Management',
+        to: '/product-list',
+        exact: false
+    }
+];
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+    return (
+        <Route
+            path={to}
+            exact={activeOnlyWhenExact}
+            children={({ match }) => {
+                var active = match ? 'active' : '';
+                return (
+                    <li className={active}>
+                        <Link to={to}>
+                            {label}
+                        </Link>
+                    </li>
+                )
+            }}
+        />
+    );
+};
 
 class Menu extends Component {
     constructor(props) {
@@ -13,15 +46,27 @@ class Menu extends Component {
             <div className="navbar navbar-default">
                 <a className="navbar-brand" href="/#">Call API</a>
                 <ul className="nav navbar-nav">
-                    <li className="active">
-                        <a href="/#">Home</a>
-                    </li>
-                    <li>
-                        <a href="/#">Products</a>
-                    </li>
+                    {this.showMenus(menus)}
                 </ul>
             </div>
         );
+    }
+
+    showMenus = (menus) => {
+        var result = null;
+        if (menus.length > 0) {
+            result = menus.map((menu, index) => {
+                return (
+                    <MenuLink
+                        key={index}
+                        label={menu.name}
+                        to={menu.to}
+                        activeOnlyWhenExact={menu.exact}
+                    />
+                );
+            });
+        }
+        return result;
     }
 }
 
