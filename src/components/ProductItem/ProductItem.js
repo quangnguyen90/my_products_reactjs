a272d54f7cb6b2f8ea00b349abb87fe08c53c956
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-16 15:07:52 +0700

    8- Delete product

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -8,6 +8,12 @@ class ProductItem extends Component {
 
     }
 
+    onDelete = (id) => {
+        if (confirm("Are you sure you want to delete this product ?")) {//eslint-disable-line
+            this.props.onDelete(id);
+        }
+    }
+
     render() {
         var { product, index } = this.props;
         var statusName = product.status ? 'On Sell' : 'Sold Out';
@@ -25,7 +31,13 @@ class ProductItem extends Component {
                 </td>
                 <td>
                     <button type="button" className="btn btn-success mr-10">Edit</button>
-                    <button type="button" className="btn btn-danger">Delete</button>
+                    <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => this.onDelete(product.id)}
+                    >
+                        Delete
+                    </button>
                 </td>
             </tr>
         );
--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -22,6 +22,31 @@ class ProductListPage extends Component {
         });
     }
 
+    findIndex = (products, id) => {
+        var result = -1;
+        products.forEach((product, index) => {
+            if (product.id === id) {
+                result = index;
+            }
+        });
+        return result;
+    }
+
+    onDelete = (id) => {
+        var { products } = this.state;
+        callApi(`products/${id}`, 'DELETE', null).then(res => {
+            if(res.status === 200) { //OK
+                var index = this.findIndex(products, id);
+                if (index  !== -1) {
+                    products.splice(index, 1);
+                    this.setState({
+                        products: products
+                    })
+                }
+            }
+        });
+    }
+
     render() {
         var { products } = this.state;
 
@@ -44,6 +69,7 @@ class ProductListPage extends Component {
                         key={index}
                         product={product}
                         index={index}
+                        onDelete={this.onDelete}
                     />
                 )
             });
